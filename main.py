793dacacfd786cde722183a85e93ff07a1eb86ea
Lothnic <joshimayank08012007@gmail.com>
2025-12-29 13:57:35 +0530

    can now print similar tokens

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from alignment.argmax import argmax_align
 from models.similarity import similarity_matrix
 
-S = similarity_matrix("Hello World","Hello World")
-print(argmax_align(S))  
+src_sentence = "the dog runs fast"
+tgt_sentence = "कुत्ता तेज़ दौड़ता है"
+
+src_tokens = src_sentence.split(" ")
+tgt_tokens = tgt_sentence.split(" ")
+
+S = similarity_matrix(src_sentence, tgt_sentence)
+aligns = argmax_align(S)
+
+for i, j in aligns:
+    print(src_tokens[i], "→", tgt_tokens[j])
